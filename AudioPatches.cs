30c137d0ffd2fa053633cddcfb7c2e386ff3e831
Dominic Aglialoro <domiaglialoro@gmail.com>
2022-01-29 00:45:31 -0800

    Toggle

--- AudioPatches.cs
+++ AudioPatches.cs
@@ -7,30 +7,60 @@ namespace SpeenPhone
 {
     public class AudioPatches
     {
-        private static bool disableHitsounds = false;
+        private static bool enableCustomSounds = true;
 
-        [HarmonyPatch(typeof(Track), nameof(Track.Update))]
-        [HarmonyPostfix]
-        private static void ToggleHitsounds()
+        private static List<FieldInfo> fields;
+        private static Dictionary<string, SoundEffect> defaultSoundEffects;
+        private static Dictionary<string, SoundEffect> customSoundEffects;
+
+        private static void SetClips() {
+            var instance = SoundEffectAssets.Instance;
+            
+            foreach (var field in fields) {
+                string name = field.Name;
+                SoundEffect soundEffect;
+
+                if (enableCustomSounds)
+                    soundEffect = customSoundEffects[name];
+                else
+                    soundEffect = defaultSoundEffects[name];
+                
+                field.SetValue(instance, soundEffect);
+            }
+        }
+
+        [HarmonyPatch(typeof(Game), nameof(Game.Update)), HarmonyPostfix]
+        private static void Game_Update_Postfix()
         {
             if (Input.GetKeyDown(KeyCode.F12))
             {
-                disableHitsounds = !disableHitsounds;
-                NotificationSystemGUI.AddMessage("Custom hitsounds are " + (disableHitsounds ? "OFF" : "ON"));
+                enableCustomSounds = !enableCustomSounds;
+                NotificationSystemGUI.AddMessage("Custom sounds are " + (enableCustomSounds ? "ON" : "OFF"));
+                SetClips();
             }
         }
 
         [HarmonyPatch(typeof(SoundEffectAssets), "CreateSoundEffectMappings"), HarmonyPrefix]
         private static void SoundEffectAssets_CreateSoundEffectMappings_Prefix(SoundEffectAssets __instance, Dictionary<string, SoundEffect> ____soundEffectMapping) {
+            fields = new List<FieldInfo>();
+            defaultSoundEffects = new Dictionary<string, SoundEffect>();
+            customSoundEffects = new Dictionary<string, SoundEffect>();
+            
             foreach (var field in typeof(SoundEffectAssets).GetFields(BindingFlags.Instance | BindingFlags.Public)) {
-                if (field.FieldType != typeof(SoundEffect) || !Main.TryGetClips(field.Name, out var clips))
-                    continue;
-
-                var soundEffect = (SoundEffect) field.GetValue(__instance);
+                string name = field.Name;
                     
+                if (field.FieldType != typeof(SoundEffect) || !Main.TryGetClips(name, out var clips))
+                    continue;
+                
+                var soundEffect = (SoundEffect) field.GetValue(__instance);
+
+                fields.Add(field);
+                defaultSoundEffects.Add(name, soundEffect);
                 soundEffect.clips = clips;
-                field.SetValue(__instance, soundEffect);
+                customSoundEffects.Add(name, soundEffect);
             }
+            
+            SetClips();
         }
     }
 }
